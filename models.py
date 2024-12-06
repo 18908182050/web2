1a1c5c806686888a8e06ed411fe876671c3e9f1d
18908182050 <f18908182050@163.com>
2024-12-06 19:48:44 +0800

    修改

--- models.py
+++ models.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+
     __tablename__ = 'users'  # 表名
     id = db.Column(db.Integer, primary_key=True)  # 主键id
     username = db.Column(db.String(250), unique=True, nullable=False)  # 唯一用户名
